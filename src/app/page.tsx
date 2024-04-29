3a470828ec7ace17c7f0bce6935b60dbc581ebf0
André Firmino <59296347+firminogg@users.noreply.github.com>
2024-04-29 00:10:22 -0300

    página de login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,32 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-3xl">CodeWise!</h1>
-      <h1>Bem vindo ao nosso Sistema de Cursos!</h1>
+    <div className="flex items-center justify-center h-screen">
+      <aside className="bg-black text-white p-6 rounded-lg w-full max-w-lg font-mono">
+        <div className="flex justify-between items-center">
+          <div className="flex space-x-2 text-red-500">
+            <div className="w-3 h-3 rounded-full bg-red-500" />
+            <div className="w-3 h-3 rounded-full bg-yellow-500" />
+            <div className="w-3 h-3 rounded-full bg-green-500" />
+          </div>
+          <p className="text-sm">bash</p>
+        </div>
+        <div className="mt-4 f">
+          <p className="text-green-400">$ login or register</p>
+          <p className="text-white">+ codewise v1.0.0</p>
+          <p className="text-white">
+            added 1 package, and audited 2 packages in 3s
+          </p>
+          <Input placeholder="username" type="email" className="w-full mt-2"></Input>
+          <Input placeholder="password" type="password" className="w-full mt-2"></Input>
+          <Button className="w-full mt-2 mb-2">login</Button>
+        </div>
+        <div className="flex justify-between">
+          <a href="#" className="text-white">registrar-se</a>
+        </div>
+      </aside>
     </div>
   );
 }
