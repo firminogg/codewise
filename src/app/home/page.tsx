63605f07e1001087b8cffac70506e6256c1222ed
André Firmino <andredd780@gmail.com>
2024-10-12 03:10:06 -0300

    login

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -8,12 +8,12 @@ import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 
 export default async function HomePage() {
-  // const session = await getServerSession();
-  // console.log(session);
+  const session = await getServerSession();
+  console.log(session);
 
-  // if (!session) {
-  //   redirect("/");
-  // }
+  if (!session) {
+    redirect("/");
+  }
   return (
     <div>
       <HeaderPage />
